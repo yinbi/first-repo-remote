88ad257ea701c9897f870d5bb920ecd7aa62b034
yinbi <86342948@qq.com>
2017-04-30 18:06:40 +0800

    add b

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -26,6 +26,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             int a = 123;
+            int b = 456;
             string datetimeStart = "2017-04-01";
             string datetimeEnd = "2017-04-06";
             DateTime dStart = Convert.ToDateTime(datetimeStart);
